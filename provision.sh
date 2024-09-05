8b12f4395d06873dae86351ce1e8c80ac3101585
halcyondream <eidolon.alpha@protonmail.com>
2024-09-05 08:48:36 -0400

    Edited configs.

--- provision.sh
+++ provision.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
+# Basic setup...
 apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
 
+# Set the host to use Port 2222.
+sudo sed -i 's/^#Port 22/Port 2222/' /etc/ssh/sshd_config && \
+sudo systemctl restart ssh
+
+# Install the official Docker.
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
-
 systemctl start docker
 systemctl enable docker
 
+# Misconfigure the local socket/daemon.
 cp /vagrant/daemon.json /etc/docker/daemon.json
 mkdir -p /etc/systemd/system/docker.service.d
 cp /vagrant/override.conf /etc/systemd/system/docker.service.d/override.conf
 systemctl daemon-reload
 systemctl restart docker
 
+# Build and launch the "orchestration" container.
 docker build -t orchestrator /vagrant/orchestrator
-docker run --privileged -d -v /var/run/docker.sock:/var/run/docker.sock -p 22:22 orchestrator
+docker run \
+    --privileged \
+    --detach \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --publish 22:22 \
+    orchestrator
